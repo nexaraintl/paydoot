a9f297630ca6937797fab609fbefcd1c4264dfb7
nexaraintl <nexaraintl@gmail.com>
2025-10-15 22:52:17 +0530

    Added new admin dashboard

--- paydoot/conn.php
+++
@@ -1,6 +0,0 @@
-<?php
- //$conn = mysqli_connect('localhost', 'u397516599_erp_tech', 'Nector@12345', 'u397516599_erp_tech');
- $conn = mysqli_connect('localhost', 'root','', 'paydoot');
-// $conn = mysqli_connect('localhost', 'u546364660_root', 'Paydoot@12345', 'u546364660_paydoot');
-
-?>
