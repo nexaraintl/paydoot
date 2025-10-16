c7f3c226277d3cdbca8a03875c1fa772b9609f74
nexaraintl <nexaraintl@gmail.com>
2025-10-17 00:33:39 +0530

    redirect to main site aftre logout admin

--- paydoot_admin/logout.php
+++ paydoot_admin/logout.php
@@ -2,6 +2,6 @@
     session_start();
     session_unset();
     session_destroy();
-    header("Location: ../paydoot_front/login.php");
+    header("Location:https://paydoot.com/");
     exit();
 ?>
